2baace14e9238ad8018f7390908ae495f755127f
anilalon <anil.alon@gmail.com>
2020-07-19 15:00:40 +0300

    removed styled component from app.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
-import classes from "./layout.css";
+import Aux from "../../hoc/Auxx";
+import classes from "./Layout.css";
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop </div>
-    <main className={classes}>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
