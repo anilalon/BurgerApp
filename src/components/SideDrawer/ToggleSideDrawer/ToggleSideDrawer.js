4e8029045ac2bbcdccdc0f9ad8a3952102240495
anilalon <anil.alon@gmail.com>
2020-07-27 14:27:31 +0300

    toggle side drawer + fix toolbar

--- src/components/SideDrawer/ToggleSideDrawer/ToggleSideDrawer.js
+++ src/components/SideDrawer/ToggleSideDrawer/ToggleSideDrawer.js
@@ -1,5 +1,12 @@
 import React from "react";
+import classes from "./ToggleSideDrawer.css";
 
-const toggleSideDrawer = (props) => <div onClick={props.clicked}>MENU</div>;
+const toggleSideDrawer = (props) => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default toggleSideDrawer;
